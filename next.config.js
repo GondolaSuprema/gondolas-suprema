802a4b2e51cdd08d7765fced14eaa621970712f1
Alessandro Thonsen <xandy@MacBook-Air.local>
2026-04-20 18:14:35 -0300

    Forçar revalidação de cache para pegar novas versões automaticamente

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
+  async headers() {
+    return [
+      {
+        source: '/:path((?!_next/static).*)',
+        headers: [{ key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' }],
+      },
+    ];
+  },
 };
 module.exports = nextConfig;
